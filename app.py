afccc4f19bd4ed657dcf62568f19be5dd2e5b72c
HowToBeCalculated <HowToBeCalculated@outlook.com>
2023-12-14 22:52:28 -0500

    add new requiments and make dev version separate`

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import dash
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+import os
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dash.page_container
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if os.getenv('DASH_ENV') == 'development':
+        app.run(debug=True)
+    else:
+        app.config.suppress_callback_exceptions = True
+        app.enable_dev_tools(debug=False)
+        app.run_server(host='0.0.0.0', port=8050)
